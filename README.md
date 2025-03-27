458cf1625f87631529266ebf05a62aa9ba87e5a0
Zane Billings <wz.billings@gmail.com>
2025-03-27 11:55:00 -0400

    chores

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ In this project I'm doing a simple test of some bayesian models on the cluster.
 
 Notes:
 
+1. Easiest way to get code onto the cluster is `scp`.
---
+++ job.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --job-name=test
+#SBATCH --ntasks=1
+#SBATCH --time=4:00:00
+#SBATCH --mem=10G
+#SBATCH --cpus-per-task=4
